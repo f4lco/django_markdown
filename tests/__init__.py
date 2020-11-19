63291c0599fc2751ccce3e2e71e5285f09e6874a
Falco Duersch <github@falcoduersch.com>
2020-11-19 15:35:46 +0100

    Replace deprecated management command 'syncdb' in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,10 +7,9 @@ from django import VERSION
 if VERSION >= (1, 7):
     from django.conf import settings
     from django.apps import apps
-
     apps.populate(settings.INSTALLED_APPS)
 
 from django.core.management import call_command
-call_command('syncdb', interactive=False)
+call_command('migrate', run_syncdb=True, interactive=False)
 
 from django_markdown.tests import * # noqa

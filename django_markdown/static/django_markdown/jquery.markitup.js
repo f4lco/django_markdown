41d82f30972703cfb6452a1fbee806a624bd6990
Falco Duersch <github@falcoduersch.com>
2020-11-19 16:55:33 +0100

    Find workaround solution for jQuery init
    This preamble must be contained in every file which assumes
    a global jQuery object.
    Previously, init of the Markdown JS parser failed due to a
    missing jQuery global. This is due to a lack of ordering
    of JS initialization: the jQuery init from django-markdown
    and the admin interface were competing. In my setup, this
    lead to following script load order:
    
    1. django-markdown jquery.init.js tries to populate a
       global jQuery object.
    2. django-admin jquery.init.js hides jQuery behind
       django.jQuery and restores the global variables to
       an a-priori seen value: undefined.
    3. jQuery.markitup.js fails, because global jQuery is
       undefined.
    
    This patch makes jQuery.markitup.js 'see' the jQuery
    instance behind django.jQuery.

--- django_markdown/static/django_markdown/jquery.markitup.js
+++ django_markdown/static/django_markdown/jquery.markitup.js
@@ -1,3 +1,7 @@
+// Starting from Django 1.6+, jQuery is only available as 'django' object attribute.
+// Unfortunately, it's not compatible with markitup.js library because it needs global jQuery variable to be initialized.
+jQuery = jQuery || django.jQuery;
+
 // ----------------------------------------------------------------------------
 // markItUp! Universal MarkUp Engine, JQuery plugin
 // v 1.1.x
